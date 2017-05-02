06788933a2d382c2bbdb53bb186199ee7798673f
applelan <alanbuscaglia@gmail.com>
2017-05-01 23:41:19 -0300

    need to add the missing fields to the database

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
     protected $fillable = [
 
         //Descomentar primera linea para caso definitivo
-        'name','lastname', 'email', 'password', 'username','birthday','adress'
+        'name','lastname', 'email', 'password', 'username','birthday', 'city_id','sales','providers','stock','clients'
         //'name','lastname','email', 'password', 'username',
     ];
 
