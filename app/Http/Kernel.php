32bc267f121f8b1b34b11936b22f519bdd742786
Alan Buscaglia <abuscaglia@belatrixsf.com>
2018-02-16 18:28:35 -0300

    cors fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,7 +50,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors' => \App\Http\Middleware\Cors::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
         'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
     ];
 }
