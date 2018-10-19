66e0d8a242fe594986ce4b8f93e67e4f7569e555
AlanBuscaglia <alanbuscaglia@gmail.com>
2018-10-19 11:29:29 -0300

    test

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,5 +52,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'cors' => \App\Http\Middleware\Cors::class,
         'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
+        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class
     ];
 }
