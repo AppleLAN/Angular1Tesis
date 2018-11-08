2b118add697c5359849e0b07f561be3b2de2857f
AlanBuscaglia <alanbuscaglia@gmail.com>
2018-11-08 20:06:08 -0300

    fix eliminacion de productos

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -98,19 +98,16 @@ class ProductsController extends Controller
     }
 
     public function deleteProducts(Request $request) {
+        $token = JWTAuth::getToken();
+        $user = JWTAuth::toUser($token);
         if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
 			// Ignores notices and reports all other kinds... and warnings
 			error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 			// error_reporting(E_ALL ^ E_WARNING); // Maybe this is enough
 		}
-        $token = JWTAuth::getToken();
-        $user = JWTAuth::toUser($token);
-
         if ($user) {
             $data = $request->all();
-            $product = Products::where('id',$data['id'])->first();
-            $product->deleted_at = Carbon::now();
-            $product->save();
+            $product = DB::table('Products')->where('id', $data['id'])->update(['deleted_at' => Carbon::now()]);
             return response()->json(['success' => 'Deleted successfully'], 200);            
         }
     }
