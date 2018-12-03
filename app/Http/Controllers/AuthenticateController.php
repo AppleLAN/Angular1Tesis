12428a151ac31cf40d03c140b009b72c75240040
AlanBuscaglia <alanbuscaglia@gmail.com>
2018-12-03 13:54:43 -0300

    nuevo intento

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -18,7 +18,7 @@ class AuthenticateController extends Controller
        // Apply the jwt.auth middleware to all methods in this controller
        // except for the authenticate method. We don't want to prevent
        // the user from retrieving their token if they don't already have it
-       $this->middleware('jwt.auth', ['except' => ['authenticateUser']]);
+       $this->middleware('jwt.auth', ['except' => ['authenticate']]);
    }
 
     public function index()
@@ -26,7 +26,7 @@ class AuthenticateController extends Controller
         
     }    
   
-    public function authenticateUser(Request $request)
+    public function authenticate(Request $request)
     {
         $credentials = $request->only('email', 'password');
         try {
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,8 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'api', 'middleware' => 'cors'], function () {
-    Route::resource('authenticate', 'AuthenticateController', ['only' => ['index']]);
-    Route::post('/authenticateUser', 'AuthenticateController@authenticateUser');
+    Route::post('/authenticate', 'AuthenticateController@authenticate');
     Route::get('/refreshToken', 'AuthenticateController@refreshToken');
     Route::get('/getID','UserController@getLoginID');
     Route::get('/getUsers','UserController@getUsers');
