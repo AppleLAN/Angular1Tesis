3e5133f1e70f273fcc37f8e7eb8b45749c70e307
AlanBuscaglia <alanbuscaglia@gmail.com>
2018-12-26 21:13:07 -0300

    cambio para arreglar aquellas que no puedieron recibir el CAE

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -166,7 +166,7 @@ class SaleController extends Controller
 		
 		$sale = Sale::find($request->input('saleId'));
 		try {
-				if ($sale->cae_data != NULL) {
+				if ($sale->cae_data != NULL && json_decode($sale->cae_data)->FeDetResp->FECAEDetResponse->CAE !== '') {
 					return response()->json(['success' => json_decode($sale->cae_data)], 200);
 				} else{
 						$afip = new Afip("wsfe");
