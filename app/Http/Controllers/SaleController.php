f929ff39133a1e6e6228d1a537a2d26908512e4b
AppleLAN <alanbuscaglia@gmail.com>
2019-02-18 22:14:26 -0300

    new test new library

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -16,8 +16,8 @@ use Tymon\JWTAuthExceptions\JWTException;
 use App\Helpers\ApiResponse;
 use Auth;
 use Carbon\Carbon;
-use App\Helpers\AFIP\Afip;
 use App\Http\Controllers\Response;
+include 'Afip.php';
 
 class SaleController extends Controller
 {
@@ -168,9 +168,8 @@ class SaleController extends Controller
 		try {
 				if (!empty($sale->cae_data) && json_decode($sale->cae_data)->FeDetResp->FECAEDetResponse->CAE !== '') {
 					return response()->json(['success' => json_decode($sale->cae_data)], 200);
-				} else{
-					try {
-						$afip = new Afip("wsfe");
+				} else {
+					/* 	$afip = new Afip("wsfe");
 					
 						$status = $afip->serverStatus();
 						if ($request->isMethod('post')) {
@@ -216,13 +215,41 @@ class SaleController extends Controller
 								$cae = $afip->requestCAE($options);
 							
 								$sale->cae_data = json_encode($cae);
-								$sale->save();
-				
-								return response()->json(['success' => $cae], 200);
-						}
-					} catch (\Exception $e) {
-						return response()->json(['error' => $e->getMessage()], 500);
-				}
+								$sale->save(); */
+
+					$data = array(
+						'CantReg' 	=> 1,  // Cantidad de comprobantes a registrar
+						'PtoVta' 	=> 1,  // Punto de venta
+						'CbteTipo' 	=> 6,  // Tipo de comprobante (ver tipos disponibles) 
+						'Concepto' 	=> 1,  // Concepto del Comprobante: (1)Productos, (2)Servicios, (3)Productos y Servicios
+						'DocTipo' 	=> 99, // Tipo de documento del comprador (99 consumidor final, ver tipos disponibles)
+						'DocNro' 	=> 0,  // Número de documento del comprador (0 consumidor final)
+						'CbteDesde' 	=> 1,  // Número de comprobante o numero del primer comprobante en caso de ser mas de uno
+						'CbteHasta' 	=> 1,  // Número de comprobante o numero del último comprobante en caso de ser mas de uno
+						'CbteFch' 	=> intval(date('Ymd')), // (Opcional) Fecha del comprobante (yyyymmdd) o fecha actual si es nulo
+						'ImpTotal' 	=> 121, // Importe total del comprobante
+						'ImpTotConc' 	=> 0,   // Importe neto no gravado
+						'ImpNeto' 	=> 100, // Importe neto gravado
+						'ImpOpEx' 	=> 0,   // Importe exento de IVA
+						'ImpIVA' 	=> 21,  //Importe total de IVA
+						'ImpTrib' 	=> 0,   //Importe total de tributos
+						'MonId' 	=> 'PES', //Tipo de moneda usada en el comprobante (ver tipos disponibles)('PES' para pesos argentinos) 
+						'MonCotiz' 	=> 1,     // Cotización de la moneda usada (1 para pesos argentinos)  
+						'Iva' 		=> array( // (Opcional) Alícuotas asociadas al comprobante
+							array(
+								'Id' 		=> 5, // Id del tipo de IVA (5 para 21%)(ver tipos disponibles) 
+								'BaseImp' 	=> 100, // Base imponible
+								'Importe' 	=> 21 // Importe 
+							)
+						), 
+					);
+					
+					$res = $afip->ElectronicBilling->CreateVoucher($data);
+					
+					$res['CAE']; //CAE asignado el comprobante
+					$res['CAEFchVto']; //Fecha de vencimiento del CAE (yyyy-mm-dd)
+	
+					return response()->json(['success' => $cae], 200);
 				}
 		} catch (\Exception $e) {
 				return response()->json(['error' => $e->getMessage()], 500);
