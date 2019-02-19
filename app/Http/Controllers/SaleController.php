b72abaa978f00e3c023fb5a81ac50cdb282c6571
AppleLAN <alanbuscaglia@gmail.com>
2019-02-18 22:18:16 -0300

    new try

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -166,91 +166,48 @@ class SaleController extends Controller
 		
 		$sale = Sale::find($request->input('saleId'));
 		try {
-				if (!empty($sale->cae_data) && json_decode($sale->cae_data)->FeDetResp->FECAEDetResponse->CAE !== '') {
-					return response()->json(['success' => json_decode($sale->cae_data)], 200);
-				} else {
-					/* 	$afip = new Afip("wsfe");
-					
-						$status = $afip->serverStatus();
-						if ($request->isMethod('post')) {
-								$pointSale = 1;
-								$type = $sale->letter; // A B C
-								$documentType = 80; //CUIT
-								$cuit = $sale->client_cuit;
-								$date = $sale->created_at;
-					
-								$lastVoucher = $afip->lastVoucher($pointSale, $type);
-								$voucherNumber = $lastVoucher['CbteNro'] + 1;
-								$voucher = $afip->voucher();
-								$voucher->voucherNumber($voucherNumber)
-												->voucherType($type)
-												->pointSale($pointSale)
-												->documentType($documentType)
-												->documentNumber($cuit)
-												->date($date)
-												->currencyType('PES')
-												->currencyTrading(1)
-												->fromDate('20180301')
-												->toDate('20181031')
-												->expirationDate('20191031');
-					
-								$product = $afip->concept();
-								$product->conceptType(1)
-												->ivaType(3)
-												->taxNet(100)
-												->taxUntaxed(0)
-												->taxExemp(0)
-												->taxIva(0);
-								$service = $afip->concept();
-								$service->conceptType(2)
-												->ivaType(3)
-												->taxNet(100)
-												->taxUntaxed(0)
-												->taxExemp(0)
-												->taxIva(0);
-					
-								$voucher->addConcept($product);
-								$voucher->addConcept($service);
-								$options = $voucher->getRequest();
-								$cae = $afip->requestCAE($options);
-							
-								$sale->cae_data = json_encode($cae);
-								$sale->save(); */
-
-					$data = array(
-						'CantReg' 	=> 1,  // Cantidad de comprobantes a registrar
-						'PtoVta' 	=> 1,  // Punto de venta
-						'CbteTipo' 	=> 6,  // Tipo de comprobante (ver tipos disponibles) 
-						'Concepto' 	=> 1,  // Concepto del Comprobante: (1)Productos, (2)Servicios, (3)Productos y Servicios
-						'DocTipo' 	=> 99, // Tipo de documento del comprador (99 consumidor final, ver tipos disponibles)
-						'DocNro' 	=> 0,  // Número de documento del comprador (0 consumidor final)
-						'CbteDesde' 	=> 1,  // Número de comprobante o numero del primer comprobante en caso de ser mas de uno
-						'CbteHasta' 	=> 1,  // Número de comprobante o numero del último comprobante en caso de ser mas de uno
-						'CbteFch' 	=> intval(date('Ymd')), // (Opcional) Fecha del comprobante (yyyymmdd) o fecha actual si es nulo
-						'ImpTotal' 	=> 121, // Importe total del comprobante
-						'ImpTotConc' 	=> 0,   // Importe neto no gravado
-						'ImpNeto' 	=> 100, // Importe neto gravado
-						'ImpOpEx' 	=> 0,   // Importe exento de IVA
-						'ImpIVA' 	=> 21,  //Importe total de IVA
-						'ImpTrib' 	=> 0,   //Importe total de tributos
-						'MonId' 	=> 'PES', //Tipo de moneda usada en el comprobante (ver tipos disponibles)('PES' para pesos argentinos) 
-						'MonCotiz' 	=> 1,     // Cotización de la moneda usada (1 para pesos argentinos)  
-						'Iva' 		=> array( // (Opcional) Alícuotas asociadas al comprobante
-							array(
-								'Id' 		=> 5, // Id del tipo de IVA (5 para 21%)(ver tipos disponibles) 
-								'BaseImp' 	=> 100, // Base imponible
-								'Importe' 	=> 21 // Importe 
-							)
-						), 
-					);
-					
-					$res = $afip->ElectronicBilling->CreateVoucher($data);
-					
-					$res['CAE']; //CAE asignado el comprobante
-					$res['CAEFchVto']; //Fecha de vencimiento del CAE (yyyy-mm-dd)
-	
-					return response()->json(['success' => $cae], 200);
+			if (!empty($sale->cae_data) && json_decode($sale->cae_data)->FeDetResp->FECAEDetResponse->CAE !== '') {
+				return response()->json(['success' => json_decode($sale->cae_data)], 200);
+			} else{
+				try {
+					if ($request->isMethod('post')) {
+						$data = array(
+							'CantReg' 	=> 1,  // Cantidad de comprobantes a registrar
+							'PtoVta' 	=> 1,  // Punto de venta
+							'CbteTipo' 	=> 6,  // Tipo de comprobante (ver tipos disponibles) 
+							'Concepto' 	=> 1,  // Concepto del Comprobante: (1)Productos, (2)Servicios, (3)Productos y Servicios
+							'DocTipo' 	=> 99, // Tipo de documento del comprador (99 consumidor final, ver tipos disponibles)
+							'DocNro' 	=> 0,  // Número de documento del comprador (0 consumidor final)
+							'CbteDesde' 	=> 1,  // Número de comprobante o numero del primer comprobante en caso de ser mas de uno
+							'CbteHasta' 	=> 1,  // Número de comprobante o numero del último comprobante en caso de ser mas de uno
+							'CbteFch' 	=> intval(date('Ymd')), // (Opcional) Fecha del comprobante (yyyymmdd) o fecha actual si es nulo
+							'ImpTotal' 	=> 121, // Importe total del comprobante
+							'ImpTotConc' 	=> 0,   // Importe neto no gravado
+							'ImpNeto' 	=> 100, // Importe neto gravado
+							'ImpOpEx' 	=> 0,   // Importe exento de IVA
+							'ImpIVA' 	=> 21,  //Importe total de IVA
+							'ImpTrib' 	=> 0,   //Importe total de tributos
+							'MonId' 	=> 'PES', //Tipo de moneda usada en el comprobante (ver tipos disponibles)('PES' para pesos argentinos) 
+							'MonCotiz' 	=> 1,     // Cotización de la moneda usada (1 para pesos argentinos)  
+							'Iva' 		=> array( // (Opcional) Alícuotas asociadas al comprobante
+								array(
+									'Id' 		=> 5, // Id del tipo de IVA (5 para 21%)(ver tipos disponibles) 
+									'BaseImp' 	=> 100, // Base imponible
+									'Importe' 	=> 21 // Importe 
+								)
+							), 
+						);
+						
+						$res = $afip->ElectronicBilling->CreateVoucher($data);
+						
+						$res['CAE']; //CAE asignado el comprobante
+						$res['CAEFchVto']; //Fecha de vencimiento del CAE (yyyy-mm-dd)				
+						return response()->json(['success' => $cae], 200);
+					}
+				} catch (\Exception $e) {
+					return response()->json(['error' => $e->getMessage()], 500);
 				}
+			}
 		} catch (\Exception $e) {
 				return response()->json(['error' => $e->getMessage()], 500);
 		}
